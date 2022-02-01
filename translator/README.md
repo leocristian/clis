57a1249a9dcf0d7f7258470ad961d8a920b07f73
leocristian <leonardosclopes@gmail.com>
2022-02-01 02:55:25 -0300

    update readme

---
+++ translator/README.md
@@ -0,0 +1,18 @@
+## Translator app
+
+An cli app for translate texts in terminal
+
+## Instalation
+
+- After download this repo, navigate to this folder and execute: `$ npm install -g .`
+
+## Usage
+
+- From terminal, execute: `$ translate -o <someCodeLang> -t <inputText>`
+
+### Params
+
+- -o = Code of the language in which the text will be translated, ex: pt (portuguese)
+- -t = Input text to be traslated (text must be in quotes)
+
+
--- translator/bin/index.js
+++ translator/bin/index.js
@@ -4,19 +4,18 @@ const yargs = require("yargs")
 const translate = require("translate-google-api")
 
 const options = yargs
- .usage("Usage: translate -i <inputCodeLang> -o <outputCodeLang> -t <inputText>")
- .option("i", { alias: "inputCodeLang", describe: "Input code language", type: "string", demandOption: true })
- .option("o", { alias: "outputCodeLang" , describe: "Output code language", type: "string", demandOption: true})
- .option("t", { alias: "inputText", describe: "Input text to translate", type: "array", demandOption: true})
+ .usage("Usage: translate -o <outputCodeLang> -t <inputText>")
+ .option("o", { alias: "outputCodeLang" , describe: "Output code language", type: "string", demandOption: false})
+ .option("t", { alias: "inputText", describe: "Input text to translate", type: "string", demandOption: false})
  .argv;
 
-
-async function translateText() {
-    console.clear()
-
+ 
+ async function translateText() {
+     console.clear()
+     
     console.log("Traduzindo texto...")
-    const result = await translate(options.t.toString(), {
-        tld: options.i,
+    const result = await translate(options.t, {
+        tld: "",
         to: options.o
     })
     console.clear()
